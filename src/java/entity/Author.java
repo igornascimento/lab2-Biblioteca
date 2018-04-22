aa4aecb3393f30f1016a23568fc9c84de99b6e55
Igor Nascimento <igornascimento@gmail.com>
2018-04-22 20:23:33 +0000

    Updating tomcat branch version to work with JPA

---
+++ src/java/entity/Author.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Igor Nascimento <igornascimento@gmail.com>
+ */
+@Entity
+@Table(name = "author")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Author.findAll", query = "SELECT a FROM Author a")
+    , @NamedQuery(name = "Author.findById", query = "SELECT a FROM Author a WHERE a.id = :id")
+    , @NamedQuery(name = "Author.findByName", query = "SELECT a FROM Author a WHERE a.name = :name")
+    , @NamedQuery(name = "Author.findBySurname", query = "SELECT a FROM Author a WHERE a.surname = :surname")
+    , @NamedQuery(name = "Author.findByCountry", query = "SELECT a FROM Author a WHERE a.country = :country")})
+public class Author implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id")
+    private Integer id;
+    
+    @Size(max = 50)
+    @Column(name = "name")
+    private String name;
+    
+    @Size(max = 50)
+    @Column(name = "surname")
+    private String surname;
+    
+    @Size(max = 50)
+    @Column(name = "country")
+    private String country;
+
+    public Author() {
+    }
+
+    public Author(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Author)) {
+            return false;
+        }
+        Author other = (Author) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Author[ id=" + id + " ]";
+    }
+    
+}
--- src/java/ws/BookWS.java
+++ src/java/ws/BookWS.java
@@ -1,101 +1,101 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ws;
-
-import java.io.IOException;
-import java.util.List;
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.MediaType;
-import model.Book;
-import repository.BooksRepository;
-
-/**
- * REST Web Service
- *
- * @author Igor Nascimento <igornascimento@gmail.com>
- */
-@Path("books")
-public class BookWS {
-
-    @Context
-    private UriInfo context;
-
-    /**
-     * Creates a new instance of BookWS
-     */
-    public BookWS() {
-    }
-    
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Book> getBooks() {
-        return BooksRepository.getInstance().list();
-    }
-    
-    @GET
-    @Path("/{code}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Book getByCode(@PathParam("code") String code) {
-        return BooksRepository.getInstance().searchByCode(code);
-    }
-    
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Book addBook(Book book, @Context final HttpServletResponse response) {
-        // adds the book
-        BooksRepository.getInstance().insertBook(book);
-        // generates the 201 header response
-        response.setStatus(HttpServletResponse.SC_CREATED);
-        
-        try {
-            response.flushBuffer();
-            //gets the book for response in json
-            return BooksRepository.getInstance().searchByCode(book.getCode());
-            
-        } catch(IOException e) {
-            // throw 500 error (?)
-            throw new WebApplicationException();
-        }
-    }
-    
-    @DELETE
-    @Path("/{code}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Book> remove(@PathParam("code") String code, @Context final HttpServletResponse response) {
-        // gets the book
-        BooksRepository instance = BooksRepository.getInstance();
-        Book b = instance.searchByCode(code);
-        
-        if (b == null) {
-            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        }
-        
-        try {
-            response.flushBuffer();
-            instance.remove(b.getCode());
-            response.setStatus(HttpServletResponse.SC_OK);
-            return instance.list();
-            
-        } catch(IOException e) {
-            // throw 500 error (?)
-            throw new WebApplicationException();
-        }
-        
-        
-    }
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package ws;
+//
+//import java.io.IOException;
+//import java.util.List;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.ws.rs.core.Context;
+//import javax.ws.rs.core.UriInfo;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.Consumes;
+//import javax.ws.rs.DELETE;
+//import javax.ws.rs.GET;
+//import javax.ws.rs.InternalServerErrorException;
+//import javax.ws.rs.POST;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.PUT;
+//import javax.ws.rs.PathParam;
+//import javax.ws.rs.core.MediaType;
+//import entity.Book;
+//import repository.BooksRepository;
+//
+///**
+// * REST Web Service
+// *
+// * @author Igor Nascimento <igornascimento@gmail.com>
+// */
+//@Path("books")
+//public class BookWS {
+//
+//    @Context
+//    private UriInfo context;
+//
+//    /**
+//     * Creates a new instance of BookWS
+//     */
+//    public BookWS() {
+//    }
+//    
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public List<Book> getBooks() {
+//        return BooksRepository.getInstance().list();
+//    }
+//    
+//    @GET
+//    @Path("/{code}")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Book getByCode(@PathParam("code") String code) {
+//        return BooksRepository.getInstance().searchByCode(code);
+//    }
+//    
+//    @POST
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Book addBook(Book book, @Context final HttpServletResponse response) {
+//        // adds the book
+//        BooksRepository.getInstance().insertBook(book);
+//        // generates the 201 header response
+//        response.setStatus(HttpServletResponse.SC_CREATED);
+//        
+//        try {
+//            response.flushBuffer();
+//            //gets the book for response in json
+//            return BooksRepository.getInstance().searchByCode(book.getCode());
+//            
+//        } catch(IOException e) {
+//            // throw 500 error
+//            throw new InternalServerErrorException();
+//        }
+//    }
+//    
+//    @DELETE
+//    @Path("/{code}")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public List<Book> remove(@PathParam("code") String code, @Context final HttpServletResponse response) {
+//        // gets the book
+//        BooksRepository instance = BooksRepository.getInstance();
+//        Book b = instance.searchByCode(code);
+//        
+//        if (b == null) {
+//            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+//        }
+//        
+//        try {
+//            response.flushBuffer();
+//            instance.remove(b.getCode());
+//            response.setStatus(HttpServletResponse.SC_OK);
+//            return instance.list();
+//            
+//        } catch(IOException e) {
+//            // throw 500 error
+//            throw new InternalServerErrorException();
+//        }
+//        
+//        
+//    }
+//}
